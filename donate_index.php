c75114e039026220e9079bb36ea63e229871ab0f
joynahiduzzaman <88341785+joynahiduzzaman@users.noreply.github.com>
2023-08-25 22:34:14 +0600

    new update

--- donate_index.php
+++ donate_index.php
@@ -12,7 +12,7 @@ require_once('db-config.php');
   <title>Donation Website</title>
   <link rel="shortcut icon" href="images/ICON/Logo.png" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <link rel="stylesheet" href="donate_style.css">
+  <link rel="stylesheet" href="donate_style1.css">
 </head>
 
 <body>
